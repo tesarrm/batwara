e2e4dfa5c90cd84e0a37409a4536df63c9c28067
Tesar Rahmat Maulana <tesarrm58@gmail.com>
2024-12-10 15:06:05 +0700

    feat: calculate equal split

--- batwara/batwara/doctype/expense/test_expense.py
+++ batwara/batwara/doctype/expense/test_expense.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, Build With Hussain and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests import IntegrationTestCase, UnitTestCase
 
 
@@ -18,7 +18,27 @@ class UnitTestExpense(UnitTestCase):
 	Use this class for testing individual functions and methods.
 	"""
 
-	pass
+	def test_equal_split_calculation(self):
+		frappe.get_doc({"doctype": "User", "email": "friend1@email.com", "first_name": "John"}).insert(
+			ignore_if_duplicate=True
+		)
+
+		frappe.get_doc({"doctype": "User", "email": "friend2@email.com", "first_name": "Jenny"}).insert(
+			ignore_if_duplicate=True
+		)
+
+		test_expense = frappe.get_doc(
+			{
+				"doctype": "Expense",
+				"description": "test expense",
+				"paid_by": "friend1@email.com",
+				"amount": 200,
+				"splits": [{"user": "friend1@email.com"}, {"user": "friend2@email.com"}],
+			}
+		).insert()
+
+		self.assertEqual(test_expense.splits[0].amount, 100)
+		self.assertEqual(test_expense.splits[1].amount, 100)
 
 
 class IntegrationTestExpense(IntegrationTestCase):
--- batwara/hooks.py
+++ batwara/hooks.py
@@ -5,6 +5,8 @@ app_description = "Simple & Open Source Splitwise Alternative"
 app_email = "buildwithhussain@gmail.com"
 app_license = "agpl-3.0"
 
+export_python_type_annotations = True
+
 # Apps
 # ------------------
 
